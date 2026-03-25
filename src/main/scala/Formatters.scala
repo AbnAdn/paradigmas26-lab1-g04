a91741809c0a5d565c4f4bc3fe4a1f4083eb3832
Lara Ayoub <lara.ayoub@mi.unc.edu.ar>
2026-03-25 14:37:04 +0000

    Integracion de downloadFeed en el Formatters

--- src/main/scala/Formatters.scala
+++ src/main/scala/Formatters.scala
@@ -2,9 +2,22 @@
 object Formatters {
 
   // Pure function to format posts from a subscription
-  def formatSubscription(url: String, posts: String): String = {
+  def formatPost(post: Post): String = {
+    s"""
+    Subreddit: ${post.subreddit}
+    Title: ${post.title}
+    Date: ${post.date}
+
+    ${post.selftext}
+    """
+  }
+  // Pure function to format posts from a subscription
+  def formatSubscription(url: String, posts: List[Post]): String = {
     val header = s"\n${"=" * 80}\nPosts from: $url \n${"=" * 80}"
-    val formattedPosts = posts.take(80)
+    val formattedPosts =  posts
+    .take(10)
+    .map(post => formatPost(post))
+    .mkString("\n\n")
     header + "\n" + formattedPosts
   }
 }
